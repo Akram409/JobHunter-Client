0a2175caee2dbd2fc9488c955912aba0e514407c
RaW <akram.iiuc.ctg@gmail.com>
2023-04-11 05:44:26 +0600

    Job Category list added

--- src/Component/Job/Job.jsx
+++ src/Component/Job/Job.jsx
@@ -1,12 +1,23 @@
-import React from 'react';
-
-const Job = ({jobs}) => {
-    console.log(jobs)
-    return (
-        <div>
-            job
+import React from "react";
+import './Job.css'
+const Job = ({ jobs }) => {
+const {category_logo,category_name,jobs_available} = jobs
+  return (
+    <>
+      <div className="card card-compact shadow-2xl temp p-7">
+        <figure>
+          <img
+            src={category_logo}
+            alt=""
+          />
+        </figure>
+        <div className="card-body">
+          <h2 className="card-title">{category_name}</h2>
+          <p>{jobs_available}</p>
         </div>
-    );
+      </div>
+    </>
+  );
 };
 
 export default Job;
--- src/Component/Jobs/Jobs.jsx
+++ src/Component/Jobs/Jobs.jsx
@@ -1,27 +1,30 @@
-import React, { useEffect, useState } from 'react';
-import Job from '../Job/Job';
+import React, { useEffect, useState } from "react";
+import Job from "../Job/Job";
 
 const Jobs = () => {
-    const [jobsData,setJobsData] = useState([])
+  const [jobsData, setJobsData] = useState([]);
 
-    useEffect(() =>{
-        fetch('Category.json')
-        .then(res => res.json())
-        .then(data => setJobsData(data))
-    },[])
-    return (
-        <div>
-            <div>
-                <h1>Job Category List</h1>
-                <p>Explore thousands of job opportunities with all the information you need. Its your future</p>
-            </div>
-            {
-                jobsData.map(jobs => <Job
-                    jobs={jobs}
-                ></Job>)
-            }
-        </div>
-    );
+  useEffect(() => {
+    fetch("Category.json")
+      .then((res) => res.json())
+      .then((data) => setJobsData(data));
+  }, []);
+  return (
+    <div className="mt-6 my-container">
+      <div className="text-center">
+        <h1 className="font-bold text-3xl my-4">Job Category List</h1>
+        <p>
+          Explore thousands of job opportunities with all the information you
+          need. Its your future
+        </p>
+      </div>
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4  gap-5 mt-10">
+        {jobsData.map((jobs) => (
+          <Job jobs={jobs}></Job>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default Jobs;
